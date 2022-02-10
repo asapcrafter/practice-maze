823db5702ec5d3bb5afca82c2a433bb301898b59
Steve <asapcrafter@gmail.com>
2022-02-09 17:22:46 -0800

    started stack algo

--- lab04.py
+++ lab04.py
@@ -1,5 +1,19 @@
 import Stack 
 
+class Stack():
+	def __init__(self):
+		self.items = []
+	def isEmpty(self):
+		return self.items == []
+	def push(self, item):
+		self.items.append(item)
+	def pop(self):
+		return self.items.pop()
+	def peek(self):
+		return self.items[len(self.items)-1]
+	def size(self):
+		return len(self.items)
+
 maze = [
 	['+','+','+','+','G','+'],
 	['+',' ','+',' ',' ','+'],
@@ -16,42 +30,55 @@ def printMaze(maze):
 	return
 
 def solveMaze(maze, startX, startY):
-	stack = Stack()
-	stack.push([startX, startY])
+	
+ stack = Stack()
 
-	# Stops the program once the goal is reached
+	### Trackers and Counters
+	global goalFound 
 	goalFound = False 
-	# Keeps track of how many valid steps were taken in the maze
+	global counter 
 	counter = 0
 
-	def checkGoal(x, y):
-		if maze[x][y] == 'G': goalFound = True; return False
+	### Maze checker 'G', '', '|'
 	def checkValid(x, y):
-		if maze[x][y] == ' ': return True; return False
+		if maze[x][y] == ' ': 
+			return True 
+		elif maze[x][y] == 'G': 
+		
+			goalFound = True
+		else: 
+	  		return False
+        
+     
 
+	### Movement functions
 	def moveForward(x, y):
+		printMaze(maze)
+		stack.push([x, y])
+		global counter
 		counter += 1
 		maze[x][y] = counter
+		checkCounterClockwise(x, y)
 
-
-	def moveBackward(x, y):
-		return
-
+	def moveBackward():
+		stack.peek() 
+		
 	def checkCounterClockwise(x, y):
+		print("Checking around")
 		def checkNorth():
-			checkValid(x + 1, y)
-			checkGoal(x + 1, y)
+			checkValid(x - 1, y)
+			checkGoal(x - 1, y)
 		def checkWest():
 			checkValid(x, y - 1)
 			checkGoal(x, y - 1)
 		def checkSouth():
-			checkValid(x - 1, y)
-			checkGoal(x - 1, y)
+			checkValid(x + 1, y)
+			checkGoal(x + 1, y)
 		def checkEast():
 			checkValid(x, y + 1)
 			checkGoal(x, y + 1)
-
 		if checkNorth() == True:
+			print("Valid")
 			moveForward(x + 1, y)
 		elif checkWest() == True:
 			moveForward(x, y - 1)
@@ -59,6 +86,15 @@ def solveMaze(maze, startX, startY):
 			moveForward(x - 1, y)
 		elif checkEast() == True:
 			moveForward(x, y + 1)
+		else:
+			moveBackward()
+   
+ 
+	moveForward(startX, startY)
+	printMaze(maze)
+	# while goalFound == False:
+ 
+solveMaze(maze, 4, 4)
 		
 			
 
